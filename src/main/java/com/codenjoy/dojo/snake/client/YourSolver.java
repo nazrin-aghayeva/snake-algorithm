e8e01eb1e8b614f565e68759d8f804008d881858
nazrin <nazrinaghayeva@gmail.com>
2020-04-11 23:23:28 +0400

    -- Snake

--- src/main/java/com/codenjoy/dojo/snake/client/YourSolver.java
+++ src/main/java/com/codenjoy/dojo/snake/client/YourSolver.java
@@ -15,7 +15,7 @@ public class YourSolver implements Solver<Board> {
 
   public static void main(String[] args) {
     WebSocketRunner.runClient(
-        "",
+            "http://167.71.44.71/codenjoy-contest/board/player/geqag1x7qs4c6mqeajag?code=6727047553447645701",
         new YourSolver(),
         new Board()
     );
